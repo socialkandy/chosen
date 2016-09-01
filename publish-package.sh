c624192c1cd125cc61e2d192fee25d6706183382
Rafael Angeline <me@rafaelangeline.com>
2016-09-01 10:21:24 -0300

    Adjust the Publish Package shell

--- publish-package.sh
+++ publish-package.sh
@@ -10,16 +10,16 @@ if [ $CURRENT_BRANCH != 'master' ] ; then
 fi
 
 CHOSEN_VERSION=`git tag --sort=v:refname | tail -1`
-GITHUB_URL=https://pfiller:${GH_TOKEN}@github.com/harvesthq/chosen-package.git
+GITHUB_URL=https://github.com/socialkandy/chosen-package.git
 
 git clone $GITHUB_URL
 rm -rf chosen-package/*
-cp README.md public/*.json public/*.png public/chosen.jquery.js public/chosen.css chosen-package/
+cp README.md public/*.png public/chosen.jquery.js public/chosen.css chosen-package/
 cp package-travis.yml chosen-package/.travis.yml
 cd chosen-package
 
-git config user.email "chosen@getharvest.com"
-git config user.name "chosen-package"
+# git config user.email "chosen@getharvest.com"
+# git config user.name "chosen-package"
 
 LATEST_VERSION=`git tag --sort=v:refname | tail -1`
 
@@ -38,4 +38,4 @@ fi
 git push origin master
 git push origin --tags
 
-echo "Chosen published to harvesthq/chosen-package"
+echo "Chosen published to socialkandy/chosen-package"
